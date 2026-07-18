98c3f0abf8791f687bc2b01bea18ebd0f4bdeda1
Shivendra78 <shivendra.24bce10389@vitbhopal.ac.in>
2026-07-18 07:28:38 +0530

    Solved problem 2178A yes or yes

---
+++ ques2178A/Main.java
@@ -0,0 +1,28 @@
+// package ques2178A;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+
+            int countY = 0;
+            for (char c : s.toCharArray()) {
+                if (c == 'Y')
+                    countY++;
+            }
+
+            if (countY <= 1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
